dd4e5c03afcc36d40a6bc853e73e2449ced45de4
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-10-15 21:55:54 +0100

    Create main menu

---
+++ src/menu.rs
@@ -0,0 +1,10 @@
+mod menu{
+    fn main_menu(){
+        println!("{:*>20}",'*');
+        println!("1. Session History");
+        println!("2. Check Firewall");
+        println!("3. Update System");
+        println!("{:->20}",'-');
+        print!("Option # ");
+    }
+}
